1cfe1137bfdbd53493d4ecb882086e20944058f5
Joe Cronin <joseph.cronin10@gmail.com>
2025-07-09 22:58:34 +0200

    small updates

--- scripts/get_weather.sh
+++ scripts/get_weather.sh
@@ -1,5 +1,6 @@
 curl "https://api.open-meteo.com/v1/forecast?latitude=52.3569&longitude=4.8008&current=temperature_2m,precipitation,cloud_cover" \
 	| jq -r '.current | [.temperature_2m, .precipitation, .cloud_cover] | @tsv' \
-	| awk '{printf " %s󰔄   %s  %s\n", $1, $2, $3}' \
+	| awk '{printf "%s %s %s\n", $1, $2, $3}' \
+	# | awk '{printf " %s󰔄   %s  %s\n", $1, $2, $3}' \
 
 # @tsv to use \t instead of \n
